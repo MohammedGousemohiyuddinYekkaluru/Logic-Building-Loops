ee5e22c01df00e0430dd0bf8f4d4424f121c2ca2
mohammed gouse <gousemohammed163@gmail.com>
2026-04-25 14:53:09 +0530

    Solve 7 & 8 questions about calculating sum of Even and Odd numbers from 1 upto n

---
+++ 08_Sum_of _Odd_Numbers.py
@@ -0,0 +1,16 @@
+# 8) Calculate the sum of all odd numbers from 1 up to n.
+
+n = 10
+
+original_n = n
+
+total_sum = 0
+
+while n >= 1:
+    if n % 2 != 0:
+        total_sum += n
+    
+    n -= 1
+
+
+print(total_sum)
