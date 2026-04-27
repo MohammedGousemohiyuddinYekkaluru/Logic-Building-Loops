2f90e89cc32783779303b039106011a182d57eb3
mohammed gouse <gousemohammed163@gmail.com>
2026-04-27 14:43:39 +0530

    Check whether the given number is an Armstrong number.

---
+++ Phase1_While_Loop/15.1_Armstrong_number.py
@@ -0,0 +1,20 @@
+# 15) Check whether the given number is an Armstrong number.
+
+## Armstrong Number :- an integer that equals the sum of its own digits, each raised to the power of the total number of digits in the number.
+
+original_num = 153
+
+temp = original_num
+
+total_sum = 0
+
+while temp > 0:
+    total_sum += (temp % 10) ** len(str(original_num))
+    temp //= 10
+
+
+if total_sum == original_num:
+    print(f"Given number {original_num} is an Armstrong number")
+
+else:
+    print(f"Given number {original_num} is not an Armstrong number")
