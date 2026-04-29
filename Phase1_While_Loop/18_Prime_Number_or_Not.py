78abe2d98b4222269f3e5333e0a62e69aacfee79
mohammed gouse <gousemohammed163@gmail.com>
2026-04-29 12:16:57 +0530

    Check whether the given number is a prime number.

--- Phase1_While_Loop/18_Prime_Number_or_Not.py
+++ Phase1_While_Loop/18_Prime_Number_or_Not.py
@@ -1,1 +1,19 @@
 # 18) Check whether the given number is a prime number.
+
+given_number = 4
+
+divisor = 2
+
+is_prime = True
+
+while divisor < given_number:
+    if given_number % divisor == 0:
+        is_prime = False
+        break
+    divisor += 1
+
+if is_prime:
+    print(f"Given number {given_number} is a prime number")
+
+else:
+    print(f"Given number {given_number} is not a prime number")
