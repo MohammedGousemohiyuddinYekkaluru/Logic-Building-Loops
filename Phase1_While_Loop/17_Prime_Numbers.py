cbe2b7ea1eaa6d62a92379ba07af0404fa6dbdec
mohammed gouse <gousemohammed163@gmail.com>
2026-04-29 12:12:01 +0530

    Print all prime numbers between 1 and 100

--- Phase1_While_Loop/17_Prime_Numbers.py
+++ Phase1_While_Loop/17_Prime_Numbers.py
@@ -1,2 +1,20 @@
 # 17) Print all prime numbers between 1 and 100
 
+number = 2
+
+while number <= 100:
+    is_prime = True
+
+    divisor = 2
+
+    while divisor < number:
+        if number % divisor == 0:
+            is_prime = False
+            break
+
+        divisor += 1
+
+    if (is_prime):
+        print(number)
+
+    number += 1
