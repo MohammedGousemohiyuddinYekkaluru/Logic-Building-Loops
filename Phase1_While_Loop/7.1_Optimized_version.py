273ae6311a168660c43847a582626b1ec15507f2
mohammed gouse <gousemohammed163@gmail.com>
2026-05-04 15:57:40 +0530

    Optimized Solution For :  Calculate the sum of all even numbers from 1 up to n.

--- Phase1_While_Loop/7.1_Optimized_version.py
+++ Phase1_While_Loop/7.1_Optimized_version.py
@@ -1,1 +1,24 @@
 # 7) Calculate the sum of all even numbers from 1 up to n.
+
+# 1) Using while loop
+
+original_num = int(input("Enter a number : "))
+
+temp = original_num - 1 if original_num % 2 != 0 else original_num
+
+total_sum = 0
+
+while temp >= 1:
+    total_sum += temp
+
+    temp -= 2
+
+print(f"The sum of Even numbers till {original_num} is {total_sum}")
+
+# 2) Using math formula (without using while loop)
+
+k = original_num // 2
+
+result_sum = k * (k + 1)
+
+print(f"The sum of Even numbers till {original_num} is {result_sum}")
