5df4194f3891ad44850197f35cc67ef2d4ae648f
mohammed gouse <gousemohammed163@gmail.com>
2026-05-05 13:34:06 +0530

    Small changes and Optimized Solution for : Check whether the given number is a palindrome.

--- Phase1_While_Loop/13_palindrome_number.py
+++ Phase1_While_Loop/13_palindrome_number.py
@@ -1,19 +1,30 @@
 # 13) Check whether the given number is a palindrome.
 
-number = int(input("Enter a number : "))
+original_number = int(input("Enter a number : "))
 
-original_number = number
+temp = abs(original_number)
 
 reverse_num = 0
 
-while number > 0:
-    last_digit = number % 10
+while temp > 0:
+    last_digit = temp % 10
     reverse_num = (reverse_num * 10) + last_digit
     
-    number //= 10
+    temp //= 10
 
 if original_number == reverse_num:
     print(f"Given Number {original_number} is a Palindrome")
 
 else:
     print(f"Given Number {original_number} is not a Palindrome")
+
+
+## Optimized Solution without using loops
+
+s = str(original_number)
+
+if s == s[::-1]:
+    print("palindrome")
+
+else:
+    print("Not a Palindrome")
