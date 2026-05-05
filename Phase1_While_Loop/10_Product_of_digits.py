6ee50f639fad86f46000473b1f49f2367d058487
mohammed gouse <gousemohammed163@gmail.com>
2026-05-05 12:57:16 +0530

    Optimized Solution for : Find and print the product of all digits of a given number.

--- Phase1_While_Loop/10_Product_of_digits.py
+++ Phase1_While_Loop/10_Product_of_digits.py
@@ -11,4 +11,21 @@ while number > 0:
 print(product_result)
 
 
+## Optimized Solution
+
+given_number = int(input("Enter a number : "))
+
+result = 0 if given_number == 0 else 1
+
+while given_number > 0:
+    last_digit = given_number % 10
+
+    if last_digit == 0:
+        result = 0
+        break
+
+    result *= last_digit
+    given_number //= 10
+
+print(result)
 
