de650f490eb82c00c23d61e2f74fecdbf435c890
mohammed gouse <gousemohammed163@gmail.com>
2026-05-05 13:05:08 +0530

    Small changes and Optimized Solution for : Count and print the total number of digits in a given number.

--- Phase1_While_Loop/11_no_of_digits.py
+++ Phase1_While_Loop/11_no_of_digits.py
@@ -1,14 +1,20 @@
 # 11) Count and print the total number of digits in a given number.
 
-number = int(input("Enter a number : "))
+original_number = int(input("Enter a number : "))
 
-original_number = number
+temp = original_number
 
-no_of_digits = 1 if number == 0 else 0 
+no_of_digits = 1 if original_number == 0 else 0 
 
-while number > 0:
+while temp > 0:
     no_of_digits += 1
-    number //= 10
-
+    temp //= 10
 
 print(f"Number of digits in {original_number} is {no_of_digits} digits")
+
+
+## Optimized Solution without using loops
+
+no_of_digits_2 = len(str(abs(original_number)))
+
+print(no_of_digits_2)
