ce47542dd5e2d3ca6a87b62b812201c7718d6ec7
mohammed gouse <gousemohammed163@gmail.com>
2026-05-05 13:13:54 +0530

    Small changes & Optimized Solution for : Reverse the given number and print the reversed value

--- Phase1_While_Loop/12_Reverse_number.py
+++ Phase1_While_Loop/12_Reverse_number.py
@@ -1,15 +1,28 @@
 # 12) Reverse the given number and print the reversed value
 
-number = int(input("Enter a number : "))
+original_number = int(input("Enter a number : "))
 
-original_number = number
+temp = abs(original_number)
 
 reverse_num = 0
 
-while number > 0:
-    last_digit = number % 10
+while temp > 0:
+    last_digit = temp % 10
     reverse_num = (reverse_num * 10) + last_digit
     
-    number //= 10
+    temp //= 10
+
+if original_number < 0:
+    reverse_num *= -1
 
 print(reverse_num)
+
+
+## Optimized Version without using loops
+
+reversed_num = int(str(abs(original_number)) [::-1])
+
+if original_number < 0:
+    reversed_num *= -1
+
+print(reversed_num)
