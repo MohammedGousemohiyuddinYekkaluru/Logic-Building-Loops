992ccfbdadff23ca6c59adfb409ccc110a9a63a9
mohammed gouse <gousemohammed163@gmail.com>
2026-05-06 13:23:35 +0530

    Small changes and Optimized Solution for: Find and print the sum of digits of the given number.

--- Phase1_While_Loop/14_Sum_of_digits.py
+++ Phase1_While_Loop/14_Sum_of_digits.py
@@ -1,14 +1,27 @@
 # 14) Find and print the sum of digits of the given number.
 
-number = int(input("Enter a number : "))
+original_num = int(input("Enter a number : "))
 
-original_num = number
+temp = original_num
 
 sum_of_digits = 0
 
-while number > 0:
-    sum_of_digits += number % 10
-    number //= 10
+while temp > 0:
+    sum_of_digits += temp % 10
+    temp //= 10
 
 
 print(f"The sum of digits of the given number {original_num} is {sum_of_digits}")
+
+
+## Optimized Solution
+
+temp2 = abs(original_num)
+
+result_sum = 0
+
+while temp2:
+    result_sum += temp2 % 10
+    result_sum //= 10
+
+print(f"The sum of digits of the given number {original_num} is {result_sum}")
