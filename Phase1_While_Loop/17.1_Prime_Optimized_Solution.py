58254dbb96539f4afbfc35e25834b105fbb2527b
mohammed gouse <gousemohammed163@gmail.com>
2026-05-07 13:25:22 +0530

    Optimized Solution for: Print all prime numbers between 1 and 100

---
+++ Phase1_While_Loop/17.1_Prime_Optimized_Solution.py
@@ -0,0 +1,25 @@
+# 17) Print all prime numbers between 1 and 100
+
+import math
+
+number = 2   # 2 is the only even prime number
+print(number)
+
+number = 3
+
+while number <= 100:
+    is_prime = True
+    limit = int(math.sqrt(number))
+    divisor = 2
+
+    while divisor < limit:
+        if number % divisor == 0:
+            is_prime = False
+            break
+
+        divisor += 1
+
+    if is_prime:
+        print(number)
+
+    number += 2  # skipping even numbers because except 2 no even number is prime
