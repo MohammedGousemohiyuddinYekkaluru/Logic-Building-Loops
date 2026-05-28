5781f6664fd3d30e4fe679274aa3aae801336fcd
mohammed gouse <gousemohammed163@gmail.com>
2026-05-28 16:54:46 +0530

    Solve printing odd and even numbers from 1-100

---
+++ Phase3_for_loop/03_even_numbers.py
@@ -0,0 +1,6 @@
+# 3) Print even numbers (1 to 100)
+
+
+for i in range(1, 101):
+    if i % 2 == 0:
+        print(i)
---
+++ Phase3_for_loop/04_odd_numbers.py
@@ -0,0 +1,6 @@
+# 4) Print odd numbers (1 to 100)
+
+
+for i in range(1, 101):
+    if i % 2 != 0:
+        print(i)
