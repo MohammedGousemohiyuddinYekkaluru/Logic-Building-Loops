10d4eb6772993276e4dcaf8eb6e597a152c7a92e
mohammed gouse <gousemohammed163@gmail.com>
2026-05-30 22:22:28 +0530

    Solve 12th & 13th problem i.e., squares & cubes upto n

---
+++ Phase3_for_loop/12_squares_1_to_n.py
@@ -0,0 +1,6 @@
+# 12) Squares from 1 to n.
+
+n = int(input("Enter a number : "))
+
+for i in range(1, n+1):
+    print(i ** 2)
---
+++ Phase3_for_loop/13_cubes_1_to_n.py
@@ -0,0 +1,6 @@
+# 13) Cubes from 1 to n.
+
+n = 5
+
+for i in range(1, n+1):
+    print(i ** 3)
