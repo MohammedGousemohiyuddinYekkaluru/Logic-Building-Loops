0b22959987a929aa476ac78d157485ada7db68e1
mohammed gouse <gousemohammed163@gmail.com>
2026-06-26 13:57:10 +0530

    Solve 4th Question: Arithmetic Progression

---
+++ Phase6_mathematical_series/04_AP.py
@@ -0,0 +1,9 @@
+# 04 - Arithmetic progression (AP).
+
+a = int(input("Enter first term of the series: "))
+n = int(input("Enter nth term : "))
+d = int(input("Enter the commom difference : "))
+
+output = a + (n - 1) * d
+
+print(output)
