dd67f523abe3b9e50849b6fe773685cc2606db58
mohammed gouse <gousemohammed163@gmail.com>
2026-06-27 14:48:21 +0530

    Solve 9th Question: Pyramid star pattern

---
+++ Phase7_star_pattern/09_pyramid_pattern.py
@@ -0,0 +1,10 @@
+# 09 - Pyramid pattern.
+
+for i in range(0, 5):
+    for j in range(5 - i - 1):
+        print("", end=" ")
+    
+    for k in range(i+1):
+        print("*", end=" ")
+
+    print()
